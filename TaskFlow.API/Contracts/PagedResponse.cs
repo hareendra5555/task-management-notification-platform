846492b4ab4f184add7a02f6a5e717f592e12bf2
Hareendra Sri Nag Nerusu <hareendra5555@gmail.com>
2026-02-21 23:30:37 -0500

    Day 4: add pagination metadata to paged responses

--- TaskFlow.API/Contracts/PagedResponse.cs
+++ TaskFlow.API/Contracts/PagedResponse.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace TaskFlow.API.Contracts;
 
@@ -8,4 +9,7 @@ public class PagedResponse<T>
     public int Page { get; set; }
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
+    public int TotalPages => PageSize <= 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page < TotalPages;
 }
