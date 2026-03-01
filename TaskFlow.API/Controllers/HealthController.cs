2f2c63b9e7ecbc910d2b495cbac9e1d85c7dcb14
Hareendra Sri Nag Nerusu <hareendra5555@gmail.com>
2026-02-28 23:33:27 -0500

    Day 5: enrich health response with environment and version

--- TaskFlow.API/Controllers/HealthController.cs
+++ TaskFlow.API/Controllers/HealthController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace TaskFlow.API.Controllers;
 
@@ -6,13 +7,24 @@ namespace TaskFlow.API.Controllers;
 [Route("api/[controller]")]
 public class HealthController : ControllerBase
 {
+    private readonly IWebHostEnvironment _environment;
+
+    public HealthController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
+        var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown";
+
         return Ok(new
         {
             status = "healthy",
             service = "taskflow-api",
+            environment = _environment.EnvironmentName,
+            version,
             timestampUtc = DateTime.UtcNow
         });
     }
